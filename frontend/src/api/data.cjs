6c0e0ba72b679405c0b42f420371fa7f9e624ffd
AbdullahTariqVS <AbdullahTariq.git@gmail.com>
2024-04-27 23:30:59 +0500

    temporary json scheme

--- frontend/src/api/data.cjs
+++ frontend/src/api/data.cjs
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const data = {
   users: [
     {
@@ -34,19 +36,58 @@ const data = {
     },
   ],
 
-  tables:[
-    
-  ], 
-    
+  roles: [
+    {
+      sessionId: 1,
+      roleId: 1,
+      roleTitle: 'Admin',
+      members: { view: true, edit: true },
+      createTable: true,
+      views: { view: [1], edit: [1] },
+      tables: { view: [1], edit: [1] },
+    }
+  ],
+
+  tables: [
+    { id: 1, sessionId: 1, title: 'Participants' }
+  ],
+
+  attributes: [
+    { id: 1, tableId: 1, title: 'id', type: 'number' },
+    { id: 2, tableId: 1, title: 'name', type: 'multi-select', values: ['jane', 'doe'] }
+  ],
+
+  tuples: [
+    {
+      id: 1, 
+      data: [
+        {attribId: 1, value: '1'}, 
+        {attribId: 2, value: 'jane'}, 
+      ]
+    }, 
+    {
+      id: 2, 
+      data: [
+        {attribId: 1, value: '2'}, 
+        {attribId: 2, value: 'doe'}, 
+      ]
+    }
+  ],
+
   relations: [
     { id: 1, fromTable: 1, toTable: 1, name: 'hostel id', onAttrib: 1, rtName: 'Participands id', rtOnAttrib: 1 },
   ],
 
+  relationData: [
+    {id: 1, rId: 1, tTupleId: 1, rTupleId: 1}, 
+    {id: 1, rId: 1, tTupleId: 1, rTupleId: 2}, 
+  ], 
   layouts: [
     {
       id: 1,
       title: 'Participants',
       tableId: 1,
+      sortBy: 1, 
       attribs: [
         { id: 1, position: 1 },
         { id: 2, position: 2 },
@@ -70,3 +111,5 @@ const data = {
     },
   ]
 }
+
+fs.writeFileSync('data.json', JSON.stringify(data));
